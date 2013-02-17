f49034243ac3c94ed98f39ad059eb3d1b1f7096d
Dan White <dwhite@olp.net>
2013-02-17 13:53:13 -0600

    first functioning version of md5

--- turon/md5.c
+++ turon/md5.c
@@ -1,10 +1,51 @@
 #include "turon/turon.h"
+#include <stdio.h>
 /*#include <string.h>
 #include <stdlib.h>*/
 
 char empty_buffer[56] = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+char empty_string[32] = "                                ";
 
-int process_512_bit_chunk (md5) {
+int process_512_bit_chunk (turon_md5_t *md5) {
+
+    int i;
+    uint32_t a, b, c, d, j, k, temp;
+    uint32_t *w;
+
+    a = md5->word_a;
+    b = md5->word_b;
+    c = md5->word_c;
+    d = md5->word_d;
+
+    w = (uint32_t *) md5->buffer;
+
+    for(i = 0; i<64; i++) {
+
+        if (i < 16) {
+            j = (b & c) | ((~b) & d);
+            k = i;
+        } else if (i < 32) {
+            j = (d & b) | ((~d) & c);
+            k = (5*i + 1) % 16;
+        } else if (i < 48) {
+            j = b ^ c ^ d;
+            k = (3*i + 5) % 16;          
+        } else {
+            j = c ^ (b | (~d));
+            k = (7*i) % 16;
+        }
+        temp = d;
+        d = c;
+        c = b;
+        b = b + TURON_MD5_ROTATE((a + j + turon_md5_rotate_array[i] + w[k]), turon_md5_shift_array[i]);
+        a = temp;
+    }
+
+    md5->word_a += a;
+    md5->word_b += b;
+    md5->word_c += c;
+    md5->word_d += d;
+ 
     return TURON_OK;
 }
 
@@ -17,10 +58,7 @@ int turon_md5_init (turon_md5_t *md5, void **data) {
     md5->length = 0;
     md5->buffer_index = 0;
 
-    int i;
-    for (i=0; i<32; i++) {
-        md5->hash_string[i] = " ";
-    }
+    memcpy (&md5->hash_string, &empty_string, 32);
     md5->hash_string[32] = "\0";
 
     return TURON_OK;
@@ -44,6 +82,7 @@ int turon_md5_feed (turon_md5_t *md5, char *buf, int buf_len) {
     if (buf_len > passed_buf_index) {
         copy_len = buf_len - passed_buf_index;
         memcpy (&md5->buffer[md5->buffer_index], &buf[passed_buf_index], copy_len);
+        md5->buffer_index += copy_len;
     } 
 
     md5->length += buf_len*8;
@@ -63,8 +102,14 @@ int turon_md5_finalize (turon_md5_t *md5) {
         md5->buffer_index = 0;
         memcpy (&md5->buffer[md5->buffer_index], &empty_buffer, 56);
     }
-    /* TODO: I need to guarantee this is in bigendian format, per RFC 1321 */
-    memcpy(&md5->buffer[56], &(md5->length), 4);
+    md5->buffer[56] = md5->length & 0x00000000000000ff;
+    md5->buffer[57] = (md5->length & 0x000000000000ff00) >> 8;
+    md5->buffer[58] = (md5->length & 0x0000000000ff0000) >> 16;
+    md5->buffer[59] = (md5->length & 0x00000000ff000000) >> 24;
+    md5->buffer[60] = (md5->length & 0x000000ff00000000) >> 32;
+    md5->buffer[61] = (md5->length & 0x0000ff0000000000) >> 40;
+    md5->buffer[62] = (md5->length & 0x00ff000000000000) >> 48;
+    md5->buffer[63] = md5->length >> 56;
     process_512_bit_chunk (md5);
     md5->buffer_index = 0;
 
